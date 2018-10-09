3302ef63a25ced5db9bd67e7e9640d49240642e9
Hisakazu Hada <hadahskz@edu.teu.ac.jp>
2018-10-09 17:47:52 +0900

    2018年度版

--- Readme.md
+++ Readme.md
@@ -1,2 +1,9 @@
 # 学部卒論用テンプレート
 
+2018年度版に改定  from gamescience.jp
+
+- latexmkrcを追加
+- .bat ファイルを削除
+- 分離したファイルを src ディレクトリに移動
+- 画像を fig ディレクトリに移動
+- includepathを設定
--- makePDF.bat
+++
@@ -1,7 +0,0 @@
-platex Thesis_Main.tex
-pbibtex Thesis_Main
-platex Thesis_Main.tex
-platex Thesis_Main.tex
-platex Thesis_Main.tex
-dvipdfmx -d 5 Thesis_Main.dvi
-call cleanFiles.bat
